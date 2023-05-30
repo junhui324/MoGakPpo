ea0e5904843c4e316196222131a5f3178b5e5713
Saem <saemilee01@gmail.com>
2023-05-30 21:25:21 +0900

    Fix: ProjectList 목데이터 수정 및  인터페이스 수정

--- src/apis/project.ts
+++ src/apis/project.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export async function getProjectList() {
-  const response = await axios.get('/mock/project-list.json');
+  const response = await axios.get('/mock/projects.json');
   return response.data;
 }
