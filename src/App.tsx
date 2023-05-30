68d38e56572c2343894bc18f06d6a05b20e9792e
Saem <saemilee01@gmail.com>
2023-05-30 22:15:50 +0900

    Add: reset.css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './reset.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Main from './pages/Main';
 import styles from './index.module.scss';
