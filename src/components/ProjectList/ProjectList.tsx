6979b808c2558aac62a84ae3bae1d952e4b47114
Saem <saemilee01@gmail.com>
2023-05-31 15:42:02 +0900

    Feat: 프로젝트 리스트 삼일 전 게시글 까지 NEW태그붙이기

--- src/components/ProjectList/ProjectList.tsx
+++ src/components/ProjectList/ProjectList.tsx
@@ -3,103 +3,100 @@ import { getProjects } from '../../apis/Fetcher';
 import { TypeProjectList } from '../../interfaces/Project.interface';
 import styles from './ProjectList.module.scss';
 
-function ProjectList() {
-  const [projectList, setProjectList] = useState<TypeProjectList[]>([]);
-  useEffect(() => {
-    const fetchData = async (): Promise<void> => {
-      try {
-        const projectList = await getProjects();
-        setProjectList(projectList);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-  }, []);
+type TypeProjectListProps = { projectList: TypeProjectList[] };
+function ProjectList({ projectList }: TypeProjectListProps) {
+  const isNewProject = (createdAt: string) => {
+    const threeDaysAgo = new Date();
+    threeDaysAgo.setDate(threeDaysAgo.getDate() - 3);
+    const createdAtDate = new Date(createdAt);
+    return createdAtDate > threeDaysAgo;
+  };
 
   return (
     <ul className={styles.container}>
-      {projectList.map((project) => {
-        const {
-          project_id,
-          project_type: type,
-          project_recruitment_status: recruitmentStatus,
-          project_title: title,
-          project_summary: summary,
-          project_recruitment_roles: recruitmentRoles,
-          project_required_stacks: requiredStacks,
-          project_goal: goal,
-          project_participation_time: participationTime,
-          project_bookmark_count: bookmarkCount,
-          project_comments_count: commentsCount,
-          project_views: viewsCount,
-          project_created_at: created_at,
-        } = project;
-        return (
-          <li key={project_id} className={styles.listContainer}>
-            <div>
+      {projectList &&
+        projectList.map((project) => {
+          const {
+            project_id,
+            project_type: type,
+            project_recruitment_status: recruitmentStatus,
+            project_title: title,
+            project_summary: summary,
+            project_recruitment_roles: recruitmentRoles,
+            project_required_stacks: requiredStacks,
+            project_goal: goal,
+            project_participation_time: participationTime,
+            project_bookmark_count: bookmarkCount,
+            project_comments_count: commentsCount,
+            project_views: viewsCount,
+            project_created_at: createdAt,
+          } = project;
+          return (
+            <li key={project_id} className={styles.listContainer}>
               <div>
-                <span className={styles.type}>{type}</span>
-                <span className={styles.goal}>{goal}</span>
+                <div>
+                  <span className={styles.type}>{type}</span>
+                  <span className={styles.goal}>{goal}</span>
+                </div>
+                {participationTime && (
+                  <span className={styles.participationTime}>{participationTime}</span>
+                )}
               </div>
-              {participationTime && (
-                <span className={styles.participationTime}>{participationTime}</span>
-              )}
-            </div>
-            <div>
-              <span
-                className={`${styles.status} ${
-                  recruitmentStatus === '모집 중'
-                    ? styles.recruiting
-                    : recruitmentStatus === '모집 완료'
-                    ? styles.done
-                    : ''
-                }`}
-              >
-                {recruitmentStatus}
-              </span>
-              <p className={styles.title}>{title}</p>
-            </div>
-            <p className={styles.summary}>{summary}</p>
-            <ul className={styles.roleContainer}>
-              {recruitmentRoles.roleList.map((role, index) => (
-                <li key={index}>{role}</li>
-              ))}
-            </ul>
-            {requiredStacks && (
-              <ul className={styles.stacksContainer}>
-                <span>기술스택</span>
-                {requiredStacks.stackList.map((stack, index) => (
-                  <li key={index}>{stack}</li>
+              <div>
+                <span
+                  className={`${styles.status} ${
+                    recruitmentStatus === '모집 중'
+                      ? styles.recruiting
+                      : recruitmentStatus === '모집 완료'
+                      ? styles.done
+                      : ''
+                  }`}
+                >
+                  {recruitmentStatus}
+                </span>
+                <p className={styles.title}>{title}</p>
+                {isNewProject(createdAt) && <span className={styles.newTag}>NEW</span>}
+              </div>
+              <p className={styles.summary}>{summary}</p>
+              <ul className={styles.roleContainer}>
+                {recruitmentRoles.roleList.map((role, index) => (
+                  <li key={index}>{role}</li>
                 ))}
               </ul>
-            )}
+              {requiredStacks && (
+                <ul className={styles.stacksContainer}>
+                  <span>기술스택</span>
+                  {requiredStacks.stackList.map((stack, index) => (
+                    <li key={index}>{stack}</li>
+                  ))}
+                </ul>
+              )}
 
-            {(bookmarkCount > 0 || commentsCount > 0 || viewsCount > 0) && (
-              <ul className={styles.countContainer}>
-                {bookmarkCount > 0 ? (
-                  <li>
-                    <span>📌</span>
-                    <span className={styles.bookmarkCount}>{bookmarkCount}</span>
-                  </li>
-                ) : undefined}
-                {commentsCount > 0 ? (
-                  <li>
-                    <span>💬</span>
-                    <span className={styles.commentsCount}>{commentsCount}</span>
-                  </li>
-                ) : undefined}
-                {viewsCount > 0 ? (
-                  <li>
-                    <span>👀</span>
-                    <span className={styles.viewsCount}>{viewsCount}</span>
-                  </li>
-                ) : undefined}
-              </ul>
-            )}
-          </li>
-        );
-      })}
+              {(bookmarkCount > 0 || commentsCount > 0 || viewsCount > 0) && (
+                <ul className={styles.countContainer}>
+                  {bookmarkCount > 0 ? (
+                    <li>
+                      <span>📌</span>
+                      <span className={styles.bookmarkCount}>{bookmarkCount}</span>
+                    </li>
+                  ) : undefined}
+                  {commentsCount > 0 ? (
+                    <li>
+                      <span>💬</span>
+                      <span className={styles.commentsCount}>{commentsCount}</span>
+                    </li>
+                  ) : undefined}
+                  {viewsCount > 0 ? (
+                    <li>
+                      <span>👀</span>
+                      <span className={styles.viewsCount}>{viewsCount}</span>
+                    </li>
+                  ) : undefined}
+                </ul>
+              )}
+            </li>
+          );
+        })}
     </ul>
   );
 }
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from 'react';
+import { getProjects } from '../../apis/Fetcher';
+import { TypeProjectList } from '../../interfaces/Project.interface';
 import Category from '../../components/ProjectList/Category';
 import ProjectList from '../../components/ProjectList/ProjectList';
 import ProjectPostButton from '../../components/ProjectList/ProjectPostButton';
@@ -5,6 +8,19 @@ import ProjectSearch from '../../components/ProjectList/ProjectSearch';
 import styles from './Main.module.scss';
 
 function Main() {
+  const [projectList, setProjectList] = useState<TypeProjectList[]>([]);
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      try {
+        const projectList = await getProjects();
+        setProjectList(projectList);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.leftContainer}>
@@ -15,7 +31,7 @@ function Main() {
       </div>
       <div className={styles.rightContainer}>
         <ProjectSearch />
-        <ProjectList />
+        <ProjectList projectList={projectList} />
       </div>
     </div>
   );
