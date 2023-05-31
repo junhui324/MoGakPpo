41583c1fbf4b02aa44e4cbafdbf9ecc0e605c71d
hyzzzzy <shinhj4068@naver.com>
2023-05-31 22:29:04 +0900

    Feat: 검색어 자동완성 기능 구현

--- src/components/Stack/index.tsx
+++ src/components/Stack/index.tsx
@@ -1,10 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, ChangeEvent } from 'react';
 import { getStackList } from '../../apis/Fetcher';
 import styles from './stack.module.scss';
 
 function Stack() {
   const [selected, setSelected] = useState<string[]>([]);
   const [stacks, setStacks] = useState<string[]>([]);
+  const [searchWord, setSearchWord] = useState<string>('');
+  const [suggestions, setSuggestions] = useState<string[]>([]);
 
   const getStackData = async () => {
     try {
@@ -15,20 +17,42 @@ function Stack() {
     }
   };
 
-  useEffect(() => {
-    getStackData();
-  }, []);
-
-  function handleDelete(stack: string) {
+  const handleDelete = (stack: string) => {
     setSelected(prevSelected => 
       prevSelected.filter(selectedStack => selectedStack !== stack));
   }
-
-  function handleAdd(stack: string) {
+    
+  const handleAdd = (stack: string) => {
     if (!selected.includes(stack)) {
       setSelected(prevSelected => [...prevSelected, stack]);
     }
   }
+  
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearchWord(value);
+
+    const filteredSuggestions = getSuggestions(value);
+    setSuggestions(filteredSuggestions);
+  };
+
+  const handleSuggestionClick = (suggestion: string) => {
+    setSearchWord(suggestion);
+    setSuggestions([]);
+    handleAdd(suggestion);
+    setSearchWord('');
+  };
+
+  const getSuggestions = (value: string) => {
+    // 로컬에 저장된 데이터를 필터링
+    return stacks.filter((item) =>
+      item.toLowerCase().startsWith(value.toLowerCase())
+    );
+  };
+
+  useEffect(() => {
+    getStackData();
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -52,7 +76,26 @@ function Stack() {
         })}
       </div>
       {/* 돋보기 svg 넣어야함 */}
-      <input className={styles.input} type='text' placeholder='기술 스택을 검색해 보세요.'/>
+      <input 
+        className={styles.input} 
+        type='text'
+        value={searchWord}
+        onChange={handleInputChange}
+        placeholder='기술 스택을 검색해 보세요.'
+      />
+      {suggestions.length > 0 && (
+        <ul className={styles.suggestionContainer}>
+          {suggestions.map((suggestion, index) => (
+            <li 
+              className={styles.suggestion}
+              key={index} 
+              onClick={() => handleSuggestionClick(suggestion)}
+            >
+              {suggestion}
+            </li>
+          ))}
+        </ul>
+      )}
       <div className={styles.title}>전체 기술 스택</div>
       <div className={styles.stackContainer}>
         {stacks.map((stack, id) => {
