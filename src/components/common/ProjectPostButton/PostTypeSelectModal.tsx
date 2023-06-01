4ad602586666c211ec411c99839a4d75b60d1ac6
Saem <saemilee01@gmail.com>
2023-06-01 23:07:16 +0900

    Feat: 글작성 버튼 URL 연결

--- src/components/common/ProjectPostButton/PostTypeSelectModal.tsx
+++ src/components/common/ProjectPostButton/PostTypeSelectModal.tsx
@@ -1,21 +1,34 @@
 import ModalBasic from '../Modal/ModalBasic';
 import styles from './PostTypeSelectModal.module.scss';
 import { BsArrowRight } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
+import ROUTES from '../../../constants/Routes';
 
 interface PostTypeSelectModalProps {
   setModalOpen: (value: boolean) => void;
 }
 const PostTypeSelectModal = ({ setModalOpen }: PostTypeSelectModalProps) => {
+  const navigate = useNavigate();
   return (
     <ModalBasic setModalOpen={setModalOpen} closeButton={false} fullScreen={true}>
       <ul className={styles.ulContainer}>
         <p>작성할 프로젝트 타입을 골라주세요.</p>
-        <li>
+        <li
+          onClick={() => {
+            navigate(`${ROUTES.CREATE}project`);
+            setModalOpen(false);
+          }}
+        >
           사이드 프로젝트
           <BsArrowRight />
         </li>
-        <li>
-          스터디
+        <li
+          onClick={() => {
+            navigate(`${ROUTES.CREATE}study`);
+            setModalOpen(false);
+          }}
+        >
+          스터디 / 모임
           <BsArrowRight />
         </li>
       </ul>
