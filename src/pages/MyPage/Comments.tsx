06199938aea9077b3957b2ffd9a611d3b7651056
hyzzzzy <shinhj4068@naver.com>
2023-06-02 01:54:29 +0900

    Feat: 댓글 클릭 시 해당 게시글로 이동 및 UI 수정

--- src/pages/MyPage/Comments.tsx
+++ src/pages/MyPage/Comments.tsx
@@ -1,11 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, MouseEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './comments.module.scss';
 import NoContentImage from '../../assets/NoContent.png';
 import { TypeUserComments } from '../../interfaces/Comment.interface';
 import { getUserComments } from '../../apis/Fetcher';
+import ROUTES from '../../constants/Routes';
 
 function Comments() {
   const [comments, setComments] = useState<TypeUserComments | []>([]);
+  const navigate = useNavigate();
 
   const getUserCommentData = async () => {
     try {
@@ -15,6 +18,12 @@ function Comments() {
       console.error('유저가 작성한 댓글을 가져오지 못했어요');
     }
   };
+
+  const handleClickComment = (event: MouseEvent<HTMLDivElement>, project_id: number) => {
+    event.preventDefault();
+
+    navigate(`${ROUTES.PROJECT}${project_id}`);
+  }
 
   useEffect(() => {
     getUserCommentData();
@@ -31,9 +40,13 @@ function Comments() {
       ) : (
         <div className={styles.comments}>
           {comments.map((data, index) => {
-            const { comment_content, comment_created_at } = data.comment;
+            const { comment_content, comment_created_at, project_id } = data.comment;
             return (
-              <div className={styles.commentWrapper} key={`${comment_created_at}-${index}`}>
+              <div 
+                className={styles.commentWrapper} 
+                key={`${comment_created_at}-${index}`}
+                onClick={(e) => handleClickComment(e, project_id)}
+              >
                 <div className={styles.comment}>{comment_content}</div>
                 <div className={styles.createdAt}>작성시간: {comment_created_at}</div>
               </div>
