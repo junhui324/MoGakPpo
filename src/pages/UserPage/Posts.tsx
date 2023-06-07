b419491828fffdf1e6b1488572b91318083c105c
cham2 <dearcham2@gmail.com>
2023-06-07 18:07:44 +0900

    Fix: 유저페이지 post컴포넌트 백엔드 api연결

--- src/pages/UserPage/Posts.tsx
+++ src/pages/UserPage/Posts.tsx
@@ -6,75 +6,68 @@ import { getUserPostsById } from '../../apis/Fetcher';
 import Project from '../../components/ProjectList/Project';
 import LoadingProject from '../../components/ProjectList/LoadingProject';
 import { useParams } from 'react-router-dom';
+import Pagination from '../../components/Pagination';
+import { useNavigate } from 'react-router-dom';
+import ROUTES from '../../constants/Routes';
 
 function Posts() {
+  const navigate = useNavigate();
   const params: { [key: string]: string | undefined } = useParams();
-  // const userId: number = params.id ? Number(params.id) : 0;
+  const userId: number = params.id ? Number(params.id) : 0;
   const [isLoading, setIsLoading] = useState(false);
+  const [currPage, setCurrPage] = useState<number>(1);
+  const [totalPageCount, setTotalPageCount] = useState<number>(0);
   const [projects, setProjects] = useState<TypeUserPosts>([]);
   const getUserPostsData = async () => {
     try {
-      // const userPostsData = await getUserPostsById(userId);
-      const userPostsData = await getUserPostsById(38);
-      setProjects(userPostsData.data.user_projects);
-      setIsLoading(true);
+      const userPostsData = await getUserPostsById(userId, currPage);
+      //@ts-ignore
+      setProjects(userPostsData.data.pagenatedProjects);
+      //@ts-ignore
+      setTotalPageCount(userPostsData.data.pageSize);
     } catch (error) {
       console.error('유저가 작성한 포스팅을 가져오지 못했어요');
+    } finally {
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
     getUserPostsData();
-  }, []);
+  }, [currPage]);
 
   return (
     <div className={styles.container}>
       <div className={styles.contentCount}>게시글 {projects.length}개</div>
       <div className={styles.posts}>
         <ul>
-          {!isLoading && <LoadingProject />}
-          {isLoading && projects.length > 0 ? (
+          {isLoading && <LoadingProject />}
+          {projects.length > 0 &&
+            !isLoading &&
             projects.map((post) => {
-              const {
-                project_id,
-                project_type,
-                project_recruitment_status,
-                project_title,
-                project_goal,
-                project_participation_time,
-                project_bookmark_count,
-                project_comments_count,
-                project_views_count,
-                project_created_at,
-              } = post;
-
-              const newProjectData = {
-                project_id,
-                project_type,
-                project_recruitment_status,
-                project_title,
-                project_goal,
-                project_participation_time,
-                project_bookmark_count,
-                project_comments_count,
-                project_views_count,
-                project_created_at,
-              };
-
               return (
                 <div key={post.project_id}>
-                  <span>프로젝트</span>
-                  <Project projectData={newProjectData} />
+                  <span className={styles.postType} onClick={() => navigate(ROUTES.PROJECT_LIST)}>
+                    프로젝트
+                  </span>
+                  <Project projectData={{ ...post }} />
                 </div>
               );
-            })
-          ) : (
-            <div>
+            })}
+          {projects.length === 0 && !isLoading && (
+            <div className={styles.noContentContainer}>
               <img className={styles.image} src={NoContentImage} alt="No Content" />
               <div className={styles.noContent}>아직 작성한 게시글이 없어요.</div>
             </div>
           )}
         </ul>
+        {projects.length > 0 && !isLoading && (
+          <Pagination
+            currPage={currPage}
+            onClickPage={setCurrPage}
+            pageCount={Math.ceil(totalPageCount)}
+          />
+        )}
       </div>
     </div>
   );
