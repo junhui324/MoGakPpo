b07372f5fecad0372c1f31dedcdc5046d5ccc0a2
hyzzzzy <shinhj4068@naver.com>
2023-06-08 21:47:01 +0900

    Refactor: 이름, 자기소개, 원하는 직군 리코일로 리팩토링

--- src/pages/UpdateUser/index.tsx
+++ src/pages/UpdateUser/index.tsx
@@ -10,18 +10,12 @@ import styles from './updateUser.module.scss';
 
 function UpdateUser() {
   const [userInfo, setUserInfo] = useRecoilState(loginAtom);
-
   const [userStack, setUserStack] = useState<string[]>([]);
   const [imageSrc, setImageSrc] = useState<string>('');
-  const [inputName, setInputName] = useState<string>('');
-  const [inputIntroLength, setInputIntroLength] = useState<number>(0);
-  const [inputCareerLength, setInputCareerLength] = useState<number>(0);
-  const [isValid, setIsValid] = useState<boolean>(true);
   const [imageFile, setImageFile] = useState<File>();
+  const [isValid, setIsValid] = useState<boolean>(true);
 
   const fileInputRef = useRef<HTMLInputElement>(null);
-  const inputIntroRef = useRef<HTMLTextAreaElement>(null);
-  const inputCareerRef = useRef<HTMLInputElement>(null);
 
   const navigate = useNavigate();
 
@@ -48,32 +42,22 @@ function UpdateUser() {
     }
   };
 
-  const handleChange = (
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-    input: React.RefObject<HTMLInputElement | HTMLTextAreaElement>,
-    max: number
-  ) => {
-    switch (input.current?.name) {
-      case 'intro': {
-        const { value } = e.target;
-        if (value.length <= max && input.current) {
-          setInputIntroLength(input.current?.value.length);
-        }
-        break;
-      }
-      case 'career': {
-        const { value } = e.target;
-        if (value.length <= max && input.current) {
-          setInputCareerLength(input.current?.value.length);
-        }
-        break;
-      }
-    }
-  };
-
   const handleSetStackList = (stacks: string[]) => {
     setUserStack(stacks);
   };
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    max: number
+  ) => {
+    const { name, value } = e.target;
+    if (value.length <= max) {
+      setUserInfo((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
+  }
 
   const handleSubmit = async (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
@@ -83,25 +67,12 @@ function UpdateUser() {
         const formData = new FormData();
 
         formData.append('user_img', imageFile as File);
-        formData.append('user_name', inputName.trim());
-        formData.append('user_introduction', inputIntroRef.current?.value as string);
-        formData.append('user_career_goal', inputCareerRef.current?.value as string);
+        formData.append('user_name', userInfo.user_name.trim());
+        formData.append('user_introduction', userInfo.user_introduction);
+        formData.append('user_career_goal', userInfo.user_career_goal);
         formData.append('user_stacks', JSON.stringify(userStack || []));
 
         await updateUserProfile(formData);
-
-        setUserInfo((prev) => {
-          return {
-            ...prev,
-            user_name: inputName.trim(),
-            user_img: imageSrc,
-            user_career_goal: inputCareerRef.current?.value as string,
-            user_stacks: {
-              stackList: userStack || []
-            },
-            user_introduction: inputIntroRef.current?.value as string
-          }
-        });
 
         navigate(`${ROUTES.MY_PAGE}`);
       } catch (error) {
@@ -133,9 +104,6 @@ function UpdateUser() {
           }
         });
         setImageSrc(data.user_img);
-        setInputName(data.user_name);
-        setInputIntroLength(data.user_introduction.length);
-        setInputCareerLength(data.user_career_goal.length);
       } catch (error) {
         console.log(error);
       }
@@ -145,8 +113,8 @@ function UpdateUser() {
   }, []);
 
   useEffect(() => {
-    setIsValid(inputName.length !== 0);
-  }, [inputName]);
+    setIsValid(userInfo.user_name.length !== 0);
+  }, [userInfo.user_name]);
 
   return (
     <div className={styles.container}>
@@ -172,38 +140,36 @@ function UpdateUser() {
             <label className={styles.name}>이름</label>
             <input
               type="text"
-              name="name"
-              value={inputName}
+              name="user_name"
+              value={userInfo.user_name}
               placeholder="이름을 입력해 주세요."
               maxLength={MAX_NAME_COUNT}
-              onChange={(e) => setInputName(e.target.value)}
+              onChange={(e) => handleChange(e, MAX_NAME_COUNT)}
             />
-            <p>{inputName.length}/{MAX_NAME_COUNT}</p>
+            <p>{userInfo.user_name.length}/{MAX_NAME_COUNT}</p>
           </div>
           <div className={styles.introContainer}>
             <label>자기소개</label>
             <textarea
-              name="intro"
-              defaultValue={userInfo.user_introduction}
-              ref={inputIntroRef}
+              name="user_introduction"
+              value={userInfo.user_introduction}
               placeholder="자기소개를 입력해 주세요."
               maxLength={MAX_INTRO_COUNT}
-              onChange={(e) => handleChange(e, inputIntroRef, MAX_INTRO_COUNT)}
+              onChange={(e) => handleChange(e, MAX_INTRO_COUNT)}
             />
-            <p>{inputIntroLength}/{MAX_INTRO_COUNT}</p>
+            <p>{userInfo.user_introduction.length}/{MAX_INTRO_COUNT}</p>
           </div>
           <div className={styles.CareerContainer}>
             <label>원하는 직군</label>
             <input
               type="text"
-              name="career"
-              defaultValue={userInfo.user_career_goal}
-              ref={inputCareerRef}
+              name="user_career_goal"
+              value={userInfo.user_career_goal}
               placeholder="원하는 직군을 입력해 주세요."
               maxLength={MAX_CAREER_COUNT}
-              onChange={(e) => handleChange(e, inputCareerRef, MAX_CAREER_COUNT)}
+              onChange={(e) => handleChange(e, MAX_CAREER_COUNT)}
             />
-            <p>{inputCareerLength}/{MAX_CAREER_COUNT}</p>
+            <p>{userInfo.user_career_goal.length}/{MAX_CAREER_COUNT}</p>
           </div>
           <Stack selectedStack={userStack} setStackList={handleSetStackList} />
           <button 
