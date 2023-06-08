df008aef255d6683e3391916223e2cd3edf01239
cham2 <dearcham2@gmail.com>
2023-06-08 19:50:14 +0900

    Feat: 메인 배너 좌우 버튼 형 캐러셀 구현

---
+++ src/components/Banner/BannerItem.ts
@@ -0,0 +1,29 @@
+import Banner1 from '../../assets/Banner/Banner1.png';
+import Banner2 from '../../assets/Banner/Banner2.png';
+import Banner3 from '../../assets/Banner/Banner3.png';
+
+const BannerItem = [
+  {
+    id: 1,
+    tag: '함께해요🤝',
+    main_title: ['오픈된 프로필을 보고', '믿음직한 팀원을 찾아보세요🔎'],
+    sub_title: ['스터디 팀원도, 프로젝트 팀원도', '모쁘에서!'],
+    img: Banner3,
+  },
+  {
+    id: 2,
+    tag: '팀 프로젝트',
+    main_title: ['팀에 합류하세요!👭'],
+    sub_title: ['우리는 함께할 때', '더 큰 일을 할 수 있습니다.'],
+    img: Banner2,
+  },
+  {
+    id: 3,
+    tag: '스터디 모집중',
+    main_title: ['나에게 딱 맞는', '프로젝트나 스터디에 참여해 보세요📖'],
+    sub_title: ['모쁘에선 수많은 팀이', '당신을 기다리고 있습니다'],
+    img: Banner1,
+  },
+];
+
+export default BannerItem;
--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,15 +1,43 @@
-import './Banner.module.scss';
-import { Player } from '@lottiefiles/react-lottie-player';
+import { useEffect, useState } from 'react';
+import styles from './Banner.module.scss';
+import BannerItem from './BannerItem';
 
 export default function Banner() {
+  const [currentId, setCurrentId] = useState(0);
+  const [move, setMove] = useState();
+
+  useEffect(() => {
+    //@ts-ignore
+    setMove(() => ({ transform: `translateX(-${currentId * 100}%)` }));
+  }, [currentId]);
+
+  const totalItems = BannerItem.length;
+
+  const handleBack = () => {
+    setCurrentId((curr) => (curr === 0 ? totalItems - 1 : curr - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentId((curr) => (curr === totalItems - 1 ? 0 : curr + 1));
+  };
+
   return (
-    <div>
-      <Player
-        autoplay
-        loop
-        src="https://assets3.lottiefiles.com/packages/lf20_UJNc2t.json"
-        style={{ height: '300px', width: '300px' }}
-      ></Player>
+    <div className={styles.slideArea}>
+      <div className={styles.bannerList} style={move}>
+        {BannerItem.map((item) => (
+          <div key={item.id} className={styles.bannerItem}>
+            <div className={styles.bannerText}>
+              <p>{item.tag}</p>
+              <h1>{item.main_title}</h1>
+              <p>{item.sub_title}</p>
+            </div>
+            <img src={item.img} alt="배너" />
+          </div>
+        ))}
+      </div>
+      <button onClick={handleBack}>◀️</button>
+      <span>{currentId + 1}</span>
+      <button onClick={handleNext}>▶️</button>
     </div>
   );
 }
