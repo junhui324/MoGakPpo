6cd13ce3f2c4a73f343732fa6d72a88a6ae8ab39
cham2 <dearcham2@gmail.com>
2023-06-08 22:59:56 +0900

    Feat:프로젝트 모집 최신글 캐러셀 기능 구현

--- src/components/ProjectList/NewPosts.tsx
+++ src/components/ProjectList/NewPosts.tsx
@@ -20,22 +20,49 @@ export default function NewPosts() {
     getProjectListData();
   }, []);
 
+  const [currentId, setCurrentId] = useState(0);
+  const [move, setMove] = useState<React.CSSProperties>();
+
+  useEffect(() => {
+    setMove(() => ({ transform: `translateX(${currentId * -100}%)` }));
+  }, [currentId]);
+
+  const totalItems = projectList.length / 2;
+
+  const handleBack = () => {
+    setCurrentId((curr) => (curr === 0 ? totalItems - 1 : curr - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentId((curr) => (curr === totalItems - 1 ? 0 : curr + 1));
+  };
+
   return (
-    <div className={styles.slideArea}>
-      <div className={styles.projectList}>
-        {projectList.map((project) => (
-          <div key={project.project_id} className={styles.project}>
-            <span>{project.project_type}</span>
-            <span>{project.project_goal}</span>
-            <h1>{project.project_title}</h1>
-            {getIsNew(project.project_created_at) && <span className={styles.newTag}>NEW</span>}
-            <h3>{project.project_summary}</h3>
-            <p>{project.project_recruitment_roles?.roleList}</p>
-            <span>👀{project.project_views_count}</span>
-            <span>💬{project.project_comments_count}</span>
-            <span>📌{project.project_bookmark_count}</span>
-          </div>
-        ))}
+    <div className={styles.newPosts}>
+      <h1>새로운 프로젝트🎉</h1>
+      <p>새로 업데이트된 모집글을 확인해보세요!</p>
+      <button>모두 보기</button>
+      <div className={styles.slideArea}>
+        <button onClick={handleBack}>◀️</button>
+        <span>{currentId + 1}</span>
+        <button onClick={handleNext}>▶️</button>
+        <div className={styles.projectList} style={move}>
+          {projectList.map((project) => (
+            <div key={project.project_id} className={styles.projectContainer}>
+              <div className={styles.project}>
+                <span>{project.project_type}</span>
+                <span>{project.project_goal}</span>
+                <h1>{project.project_title}</h1>
+                {getIsNew(project.project_created_at) && <span className={styles.newTag}>NEW</span>}
+                <h3>{project.project_summary}</h3>
+                <p>{project.project_recruitment_roles?.roleList}</p>
+                <span>👀{project.project_views_count}</span>
+                <span>💬{project.project_comments_count}</span>
+                <span>📌{project.project_bookmark_count}</span>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
