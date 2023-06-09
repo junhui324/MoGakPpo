898fe857fd5f821cbe75986bedaa318b3ac9aed3
hyzzzzy <shinhj4068@naver.com>
2023-06-09 15:07:37 +0900

    Design: 포트폴리오, 포스트 UI 수정

--- src/components/Home/HotPortfolio/index.tsx
+++ src/components/Home/HotPortfolio/index.tsx
@@ -3,6 +3,7 @@ import { getIsNew } from '../../../utils/getIsNew';
 import { useState, useEffect } from 'react';
 import styles from './HotPortfolio.module.scss';
 import { TypePortfolioList } from '../../../interfaces/Portfolio.interface';
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 export default function HotPortfolio() {
   const [portfolioList, setPortfolioList] = useState<TypePortfolioList[]>([]);
@@ -39,13 +40,14 @@ export default function HotPortfolio() {
 
   return (
     <div className={styles.HotPortfolio}>
-      <h1>인기 포트폴리오🎉</h1>
-      <p>인기 포트폴리오 자랑글을 확인해보세요!</p>
-      <button>모두 보기</button>
+      <div className={styles.titleContainer}>
+        <div className={styles.titleTextWrapper}>
+          <h1>인기 포트폴리오🎉</h1>
+          <p>인기 포트폴리오 자랑글을 확인해보세요!</p>
+        </div>
+        <button>모두 보기</button>
+      </div>
       <div className={styles.slideArea}>
-        <button onClick={handleBack}>◀️</button>
-        <span>{currentId + 1}</span>
-        <button onClick={handleNext}>▶️</button>
 
         <div className={styles.portfolioList} style={move}>
           {portfolioList.map((portfolio) => (
@@ -59,16 +61,34 @@ export default function HotPortfolio() {
                   }
                   alt="포트폴리오 썸네일"
                 />
-                <h1>{portfolio.title}</h1>
-                <h3>{portfolio.summary}</h3>
-                <p>{portfolio.stack.stackList}</p>
-                <span>👀{portfolio.views}</span>
-                <span>💬{portfolio.comments}</span>
-                <span>📌{portfolio.bookmarks}</span>
+                <div className={styles.contentWrapper}>
+                  <h1 className={styles.title}>{portfolio.title}</h1>
+                  <h3 className={styles.summary}>{portfolio.summary}</h3>
+                  <div className={styles.stack}>
+                    {portfolio.stack.stackList?.map((stack, index) => (
+                      <p key={`${stack}-${index}`}>{stack}</p>
+                    ))}
+                  </div>
+                  <div className={styles.viewWrapper}>
+                    <span>👀</span>
+                    <span className={styles.count}>{portfolio.views}</span>
+                    <span>💬</span>
+                    <span className={styles.count}>{portfolio.comments}</span>
+                    <span>📌</span>
+                    <span className={styles.count}>{portfolio.bookmarks}</span>
+                  </div>
+                </div>
               </div>
             </div>
           ))}
         </div>
+
+        <button className={styles.leftArrow} onClick={handleBack}>
+          <IoIosArrowBack />
+        </button>
+        <button className={styles.rightArrow} onClick={handleNext}>
+          <IoIosArrowForward />
+        </button>
       </div>
     </div>
   );
