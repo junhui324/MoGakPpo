c75bbbcd4485317bee28d57293e5ad64872fb406
hyzzzzy <shinhj4068@naver.com>
2023-06-09 14:29:40 +0900

    Design: Post UI 수정

--- src/components/Home/NewPosts/index.tsx
+++ src/components/Home/NewPosts/index.tsx
@@ -3,6 +3,7 @@ import { getProjects } from '../../../apis/Fetcher';
 import { TypeProjectList } from '../../../interfaces/Project.interface';
 import styles from './NewPosts.module.scss';
 import { getIsNew } from '../../../utils/getIsNew';
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 export default function NewPosts() {
   const [projectList, setProjectList] = useState<TypeProjectList[]>([]);
@@ -39,30 +40,48 @@ export default function NewPosts() {
 
   return (
     <div className={styles.newPosts}>
-      <h1>새로운 프로젝트🎉</h1>
-      <p>새로 업데이트된 모집글을 확인해보세요!</p>
-      <button>모두 보기</button>
+      <div className={styles.titleContainer}>
+        <div className={styles.titleTextWrapper}>
+          <h1>새로운 프로젝트🎉</h1>
+          <p>새로 업데이트된 모집글을 확인해보세요!</p>
+        </div>
+        <button>모두 보기</button>
+      </div>
       <div className={styles.slideArea}>
-        <button onClick={handleBack}>◀️</button>
-        <span>{currentId + 1}</span>
-        <button onClick={handleNext}>▶️</button>
         <div className={styles.projectList} style={move}>
           {projectList.map((project) => (
             <div key={project.project_id} className={styles.projectContainer}>
               <div className={styles.project}>
-                <span>{project.project_type}</span>
-                <span>{project.project_goal}</span>
-                <h1>{project.project_title}</h1>
-                {getIsNew(project.project_created_at) && <span className={styles.newTag}>NEW</span>}
-                <h3>{project.project_summary}</h3>
-                <p>{project.project_recruitment_roles?.roleList}</p>
-                <span>👀{project.project_views_count}</span>
-                <span>💬{project.project_comments_count}</span>
-                <span>📌{project.project_bookmark_count}</span>
+                <span className={styles.type}>{project.project_type}</span>
+                <span className={styles.goal}>{project.project_goal}</span>
+                <div className={styles.titleWrapper}>
+                  <h1 className={styles.title}>{project.project_title}</h1>
+                  {getIsNew(project.project_created_at) && <span className={styles.newTag}>NEW</span>}
+                </div>
+                <h3 className={styles.summary}>{project.project_summary}</h3>
+                <div className={styles.role}>
+                  {project.project_recruitment_roles?.roleList?.map((role, index) => (
+                    <p key={`${role}-${index}`}>{role}</p>
+                  ))}
+                </div>
+                <div className={styles.viewWrapper}>
+                  <span>👀</span>
+                  <span className={styles.count}>{project.project_views_count}</span>
+                  <span>💬</span>
+                  <span className={styles.count}>{project.project_comments_count}</span>
+                  <span>📌</span>
+                  <span className={styles.count}>{project.project_bookmark_count}</span>
+                </div>
               </div>
             </div>
           ))}
         </div>
+        <button className={styles.leftArrow} onClick={handleBack}>
+          <IoIosArrowBack />
+        </button>
+        <button className={styles.rightArrow} onClick={handleNext}>
+          <IoIosArrowForward />
+        </button>
       </div>
     </div>
   );
