b81b542c124891c88cc2969f1869b6d9000169f6
saem <saemilee01@gmail.com>
2023-06-11 06:14:18 +0900

    Refactor: 필요 없는 조건문 및 테스트 함수 제거

--- src/pages/ProjectList/index.tsx
+++ src/pages/ProjectList/index.tsx
@@ -30,10 +30,12 @@ function ProjectListMain() {
           pageCount
         );
         if (isPagenation) {
+          // 무한스크롤을 위한 다음 페이지 데이터 get
           pageSize <= pageCount && setMoreData(false);
           setProjectList((prev) => [...prev, ...projectList.data.pagenatedProjects]);
           setPageCount((prev) => prev + 1);
         } else {
+          // 카테고리/모집 중/검색어 필터 변경 시 새로운 데이터 get
           const pageSize = projectList.data.pageSize;
           setPageSize(pageSize);
           // 가져온 프로젝트 리스트 사이즈가 1일 경우 moreDat 컴포넌트 렌더링x
@@ -48,40 +50,29 @@ function ProjectListMain() {
           setProjectList([]);
         }
       } finally {
-        // setPageCount((prev) => prev + 1);
         setIsLoading(false);
       }
     },
-    [selectedCategory, recruitingFilter, keywordValue, pageCount]
+    [selectedCategory, recruitingFilter, keywordValue, pageCount, pageSize]
   );
-
-  // const getNextProjectListData = useCallback(async (): Promise<void> => {
-  //   try {
-  //     const projectList = await getProjects(
-  //       selectedCategory,
-  //       recruitingFilter,
-  //       keywordValue,
-  //       pageCount
-  //     );
-  //     //토탈 페이지 수의 전 페이지일 경우 moreData=false로 세팅해서 하단 로딩 컴포넌트 안보이게하기
-  //     pageSize <= pageCount && setMoreData(false);
-  //     setProjectList((prev) => [...prev, ...projectList.data.pagenatedProjects]);
-  //     setPageCount((prev) => prev + 1);
-  //   } catch (error: any) {
-  //     if (error.message === '404') {
-  //       setMoreData(false);
-  //       setIsLoading(false);
-  //       setProjectList([]);
-  //     }
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // }, [selectedCategory, recruitingFilter, keywordValue, pageCount]);
 
   const target = useInfiniteScroll(async (entry, observer) => {
     //토탈 페이지 수의 페이지까지만 다음 페이지 데이터 업데이트하기
     pageSize >= pageCount && (await getProjectListData(true));
   });
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    getProjectListData();
+  }, [selectedCategory, recruitingFilter]);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      window.scroll(0, 0);
+      getProjectListData();
+    }, 700); // 디바운스 타임 설정
+    return () => clearTimeout(delayDebounceFn);
+  }, [keywordValue]);
 
   const handleCategoryClick = async (key: string) => {
     setSelectedCategory(key);
@@ -103,29 +94,6 @@ function ProjectListMain() {
     setPageCount(1);
     setMoreData(true);
   };
-
-  useEffect(() => {
-    window.scroll(0, 0);
-    getProjectListData();
-  }, [selectedCategory, recruitingFilter]);
-
-  useEffect(() => {
-    const delayDebounceFn = setTimeout(() => {
-      window.scroll(0, 0);
-      if (isSearched === true) {
-        getProjectListData();
-      }
-    }, 700); // 디바운스 타임 설정
-    return () => clearTimeout(delayDebounceFn);
-  }, [keywordValue, isSearched]);
-
-  // useEffect(() => {
-  //   console.log('1');
-  // }, [getNextProjectListData]);
-
-  useEffect(() => {
-    console.log('2');
-  }, [getProjectListData]);
 
   return (
     <div className={styles.container}>
