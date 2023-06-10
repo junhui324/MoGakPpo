677a49192df143cd7f6cba5282477df4a27db22d
saem <saemilee01@gmail.com>
2023-06-11 02:11:25 +0900

    Feat: 로그인 한 유저 글이 아닌 경우 수정 페이지 접근 불가

--- src/pages/PortfolioModify/index.tsx
+++ src/pages/PortfolioModify/index.tsx
@@ -3,25 +3,34 @@ import PortfolioWritingForm from '../../components/PortfolioWritingForm/Portfoli
 import * as Fetcher from '../../apis/Fetcher';
 import { useEffect, useState } from 'react';
 import * as Interface from '../../interfaces/Portfolio.interface';
+import { loginAtom } from '../../recoil/loginState';
+import { useRecoilValue } from 'recoil';
+import { useNavigate } from 'react-router-dom';
+import ROUTES from '../../constants/Routes';
 
 function PortfolioWriting() {
+  const navigate = useNavigate();
+  const loginData = useRecoilValue(loginAtom);
   const params = useParams();
   const [portflioData, setPortfolioData] = useState<Interface.TypePortfolioDetail>();
   const getPortfolioData = async () => {
+    console.log(params.id);
+    console.log(loginData.user_id);
     try {
       const response = await Fetcher.getPortfolio(params.id!);
       const data = response.data;
-      return data;
+      if (data.user_id === loginData.user_id) {
+        setPortfolioData(data);
+      } else {
+        alert('접근 권한이 없습니다.');
+        navigate(ROUTES.PORTFOLIO_LIST);
+      }
     } catch (e) {
       console.log(e);
     }
   };
   useEffect(() => {
-    const fetchData = async () => {
-      const data = await getPortfolioData();
-      setPortfolioData(data);
-    };
-    fetchData();
+    getPortfolioData();
   }, []);
 
   useEffect(() => {
