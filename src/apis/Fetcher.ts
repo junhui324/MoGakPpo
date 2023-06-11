790d33878fc44ba40a02eceb23e39e6de0fe08fd
junhui324 <junhuijang00@gmail.com>
2023-06-11 15:39:56 +0900

    Feat: 포트폴리오 북마크 api

--- src/apis/Fetcher.ts
+++ src/apis/Fetcher.ts
@@ -63,6 +63,27 @@ export async function deleteProjectBookmark(projectId: number): Promise<{ bookma
   return response.data;
 }
 
+export async function postPortfolioBookmark(protfolioId: number): Promise<{ bookmark_id: number }> {
+  // 비회원 오류 이슈가 있었으므로 추가하였음.
+  if (!Token.getToken()) throw new Error('로그인이 필요한 요청입니다.');
+
+  const params = `bookmarks/portfolio`;
+  const data = {
+    portfolio_id: protfolioId,
+  };
+  const response: AxiosResponse = await Api.post(API_KEY, params, data);
+  return response.data;
+}
+
+// 포트폴리오 북마크 취소
+export async function deletePortfolioBookmark(
+  protfolioId: number
+): Promise<{ bookmark_id: number }> {
+  const params = `bookmarks/portfolio/${protfolioId}`;
+  const response: AxiosResponse = await Api.delete(API_KEY, params);
+  return response.data;
+}
+
 // 코멘트 리스트 불러오기
 export async function getComment(
   projectId: number,
