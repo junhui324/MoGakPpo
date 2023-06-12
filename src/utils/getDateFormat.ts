b46e0267b0a0f3aa662eba788b6fb953db16642e
hyzzzzy <shinhj4068@naver.com>
2023-06-13 01:55:46 +0900

    Fix: 날짜 함수 수정, 회원 정보 null 일때 에러 처리, 이미지 수정 안했을때 에러 처리

--- src/utils/getDateFormat.ts
+++ src/utils/getDateFormat.ts
@@ -1,19 +1,19 @@
 const getKorDay = (day: number) => {
   switch (day) {
     case 0:
-      return '월요일';
-    case 1:
-      return '화요일';
-    case 2:
-      return '수요일';
-    case 3:
-      return '목요일';
-    case 4:
-      return '금요일';
-    case 5:
-      return '토요일';
-    case 6:
       return '일요일';
+    case 1:
+      return '월요일';
+    case 2:
+      return '화요일';
+    case 3:
+      return '수요일';
+    case 4:
+      return '목요일';
+    case 5:
+      return '금요일';
+    case 6:
+      return '토요일';
   }
 };
 
@@ -26,14 +26,13 @@ const checkAmPm = (hour: number) => {
 
 const getDateFormat = (createdAt: string) => {
   const newDate = new Date(createdAt);
-  const localDate = new Date(newDate.getTime() - newDate.getTimezoneOffset() * 60000);
 
   return `
-    ${localDate.getFullYear()}년 
-    ${Number(localDate.getMonth()) + 1}월 
-    ${localDate.getDate()}일
-    ${getKorDay(localDate.getDay())}
-    ${checkAmPm(localDate.getHours())}:${localDate.getMinutes()}:${localDate.getSeconds()}
+    ${newDate.getFullYear()}년 
+    ${Number(newDate.getMonth()) + 1}월 
+    ${newDate.getDate()}일
+    ${getKorDay(newDate.getDay())}
+    ${checkAmPm(newDate.getHours())}:${newDate.getMinutes()}:${newDate.getSeconds()}
   `;
 };
 
