0958587e0f0bbf51604d4cb2a7ab216b68987770
cham2 <dearcham2@gmail.com>
2023-06-13 00:01:12 +0900

    Feat: 유저페이지에서 포폴리스트도 조회가능

---
+++ src/pages/UserPage/ContentsFilter.tsx
@@ -0,0 +1,20 @@
+import styles from './contentsFilter.module.scss';
+
+interface ContentsFilterProps {
+  onChange: (value: string) => void;
+}
+
+function ContentsFilter({ onChange }: ContentsFilterProps) {
+  return (
+    <div className={styles.container}>
+      <label>
+        <select onChange={(e) => onChange(e.target.value)}>
+          <option value="project">프로젝트</option>
+          <option value="portfolio">포트폴리오</option>
+        </select>
+      </label>
+    </div>
+  );
+}
+
+export default ContentsFilter;
