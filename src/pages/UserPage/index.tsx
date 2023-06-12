886edce5fc07d52e7d9849a87d24c19a453b8f49
cham2 <dearcham2@gmail.com>
2023-06-13 00:20:00 +0900

    Remove: userPage에서 tab삭제

--- src/pages/UserPage/index.tsx
+++ src/pages/UserPage/index.tsx
@@ -1,16 +1,10 @@
 import { useEffect, useState } from 'react';
 import Profile from '../../components/UserProfile';
-import Tab from './Tab';
 import Posts from './Posts';
 
 function UserPage() {
-  const tabs = ['게시글'];
-  const [currTab, setCurrTab] = useState<string>('게시글');
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
-  function handleClickTab(tab: string) {
-    setCurrTab(tab);
-  }
   function handleContentsError(message: string) {
     setErrorMessage(message);
   }
@@ -22,8 +16,7 @@ function UserPage() {
   return (
     <div>
       <Profile />
-      <Tab tabs={tabs} currTab={currTab} onClick={handleClickTab} />
-      {!errorMessage && currTab === '게시글' && <Posts onError={handleContentsError} />}
+      {!errorMessage && <Posts onError={handleContentsError} />}
     </div>
   );
 }
