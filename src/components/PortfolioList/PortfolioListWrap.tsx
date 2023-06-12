c2424b6490bc0972064dfc27934b303f068c2fc1
JuYoung Lee <lee_ju_0@naver.com>
2023-06-12 17:59:36 +0900

    Refactor: 포트폴리오 리스트 부분 메모이제이션 및 useEffect의존성 배열 수정

--- src/components/PortfolioList/PortfolioListWrap.tsx
+++ src/components/PortfolioList/PortfolioListWrap.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useMemo, useCallback } from 'react';
+import debounce from '../../utils/debounce';
 
 // component
 import PortfolioCell from './PortfolioCell';
@@ -11,9 +12,11 @@ import { TypePortfolioList } from '../../interfaces/Portfolio.interface';
 
 // 스타일
 import styles from './PortfolioListWrap.module.scss';
+import Loading from '../common/Loading/Loading';
 
 // 상수
 const INITIAL_PAGE = 1;
+const DEBOUNCING = 100;
 
 function PortfolioListWrap({ keyword }: { keyword: string }) {
   // 상태관리
@@ -22,23 +25,9 @@ function PortfolioListWrap({ keyword }: { keyword: string }) {
   const [totalPage, setTotalPage] = useState<number>(INITIAL_PAGE);
   const page = useRef<number>(INITIAL_PAGE);
 
-  // Intersection Oberserver API
-  const targetRef = useRef<HTMLDivElement>(null); // 옵저버 엔트리
-  const options = {
-    // 기본 root인 사용자 뷰포트로 관찰
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.8,
-  };
-  const infiniteScroll = (entries: IntersectionObserverEntry[]) => {
-    !isLoading && page.current <= totalPage && fetchData();
-  };
-  let observer: IntersectionObserver;
-
   const fetchData = async () => {
     // 로딩중 상태를 true로 변경합니다.
     setIsLoading(true);
-    console.log(page, totalPage);
 
     try {
       const data = await Fetcher.getPortfolioList(page.current, keyword);
@@ -46,8 +35,8 @@ function PortfolioListWrap({ keyword }: { keyword: string }) {
       // 데이터에 맞게 페이지 최대 사이즈와 데이터를 설정합니다.
       setTotalPage(data.pageSize);
       setData((prev) => {
-        if (prev) return [...prev, ...data.pagenatedPortfolio];
-        return data.pagenatedPortfolio;
+        if (prev) return [...prev, ...data.pagenatedPortfolios];
+        return data.pagenatedPortfolios;
       });
 
       // 페이지 위치 증가
@@ -60,46 +49,70 @@ function PortfolioListWrap({ keyword }: { keyword: string }) {
     }
   };
 
-  useEffect(() => {
-    fetchData();
+  // Intersection Oberserver API
+  let observerRef = useRef<IntersectionObserver | null>(null); // 옵저버
+  const targetRef = useRef<HTMLDivElement>(null); // 옵저버 엔트리
+  const options = useMemo(() => {
+    // 상태와 관련된 값이기 때문에 useMemo 사용
+    return {
+      // 기본 root인 사용자 뷰포트로 관찰
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.8,
+    };
   }, []);
 
-  // useEffect(() => {
-  //   // 키워드가 입력되면 모든 결과를 초기화합니다.
-  //   setData(null);
-  //   setTotalPage(INITIAL_PAGE);
-  //   page.current = INITIAL_PAGE;
+  // 상태와 관련된 값을 이용하는 함수를 반환하므로 useCallback사용
+  const infiniteScroll = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      // target을 교차했을 때
+      if (entries[0].isIntersecting) {
+        !isLoading && page.current <= totalPage && fetchData();
+      }
+    },
+    [fetchData, totalPage, isLoading]
+  );
 
-  //   fetchData();
-  // }, [keyword]);
+  const keywordSearch = () => {
+    // 키워드가 입력되면 모든 결과를 초기화합니다.
+    setData(null);
+    setTotalPage(INITIAL_PAGE);
+    page.current = INITIAL_PAGE;
 
-  // totalPage가 갱신될때마다 observer 갱신
+    fetchData();
+  };
+
+  // 초기 로딩을 포함합니다.
+  useEffect(() => {
+    keywordSearch();
+  }, [keyword]);
+
+  // 상태가 변할때, 메모이제이션을 통한 값들이 변하면 observer 갱신
   useEffect(() => {
     // 새로운 옵저버 생성. totalPage 값을 갱신해주기 위함.
-    observer = new IntersectionObserver(infiniteScroll, options);
+    // 너무 빠른 target 인식을 피하기 위해 디바운싱 진행.
+    observerRef.current = new IntersectionObserver(debounce(infiniteScroll, DEBOUNCING), options);
     // 옵저버 등록
-    targetRef.current && observer.observe(targetRef.current);
+    targetRef.current && observerRef.current.observe(targetRef.current);
 
     return () => {
       // 갱신할 때 기존 observer를 disconnet함
-      observer.disconnect();
+      observerRef.current && observerRef.current.disconnect();
     };
-  }, [totalPage]);
+  }, [infiniteScroll, options]);
 
   return (
     <>
       <div className={styles.container}>
         {/* 로딩되어있는 데이터 표시 */}
-        {data ? (
-          data.map((portfolio) => {
-            return <PortfolioCell key={portfolio.portfolio_id} portfolio={portfolio} />;
-          })
-        ) : (
-          <p>'포스트없음'</p>
-        )}
-        {/* 로딩 중일때는 현재 내용에 로딩중 컴포넌트를 붙입니다. */}
-        {isLoading && <PortfolioCell isLoading={true} />}
+        {data
+          ? data.map((portfolio) => {
+              return <PortfolioCell key={portfolio.portfolio_id} portfolio={portfolio} />;
+            })
+          : !isLoading && <p>'포스트없음'</p>}
       </div>
+      {/* 로딩 중일때는 현재 내용에 로딩중 컴포넌트를 붙입니다. */}
+      {isLoading && <Loading />}
       <div className={styles.observer} ref={targetRef}></div>
     </>
   );
