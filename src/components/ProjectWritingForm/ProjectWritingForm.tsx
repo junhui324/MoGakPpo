10b7f61b27c5e31edf46391f620c6b30ba88dbeb
junhui324 <junhuijang00@gmail.com>
2023-06-12 18:18:00 +0900

    Fix: 이미지 도메인 수정

--- src/components/ProjectWritingForm/ProjectWritingForm.tsx
+++ src/components/ProjectWritingForm/ProjectWritingForm.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 import RadioButton from './RadioButton';
 import Checkbox from './Checkbox';
 import styles from './ProjectWritingForm.module.scss';
