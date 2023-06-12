2e0713e5a73333a50bfa5f559ffca33912015c17
JuYoung Lee <lee_ju_0@naver.com>
2023-06-12 17:44:09 +0900

    Style: 밑 부분 경계선 작성, 북마크 버튼 추가 등

--- src/components/PortfolioList/PortfolioCell.tsx
+++ src/components/PortfolioList/PortfolioCell.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import * as Fetcher from '../../apis/Fetcher';
 
 //타입
 import { TypePortfolioList } from '../../interfaces/Portfolio.interface';
@@ -8,6 +9,7 @@ import { TypePortfolioList } from '../../interfaces/Portfolio.interface';
 import styles from './PortfolioCell.module.scss';
 import ROUTES from '../../constants/Routes';
 import { getIsNew } from '../../utils/getIsNew';
+import { BsBookmark, BsBookmarkFill } from 'react-icons/bs';
 
 function PortfolioCell({
   isLoading = false,
@@ -32,14 +34,48 @@ function PortfolioCell({
 
   if (!portfolio) return <></>;
 
+  const handlePortfolioConfirm = () => {
+    navigate(`${ROUTES.PORTFOLIO_DETAIL}${portfolio.portfolio_id}`);
+    // 스크롤을 맨 위로 올립니다.
+    window.scrollTo(0, 0);
+  };
+
+  const handleBookmark = async () => {
+    try {
+      portfolio.is_bookmarked
+        ? await Fetcher.deletePortfolioBookmark(portfolio.portfolio_id)
+        : await Fetcher.postPortfolioBookmark(portfolio.portfolio_id);
+    } catch (error) {
+      if (error instanceof Error && typeof error.message === 'string') {
+        switch (error.message) {
+          case '401':
+            alert('로그인 후 사용 가능합니다.');
+            navigate(`${ROUTES.LOGIN}`);
+            break;
+          default:
+            alert('예기치 않은 오류가 발생했습니다.');
+            return;
+        }
+      }
+    }
+  };
+
   return (
-    <div className={styles.container} onClick={() => navigate(`${ROUTES.PORTFOLIO_DETAIL}${87}`)}>
+    <div className={styles.container}>
+      <button className={styles.bookmarkButton} onClick={handleBookmark}>
+        {portfolio.is_bookmarked ? <BsBookmarkFill /> : <BsBookmark />}
+      </button>
       {portfolio.portfolio_thumbnail ? (
-        <img className={styles.image} src={portfolio.portfolio_thumbnail} alt="포트폴리오 섬네일" />
+        <img
+          className={styles.image}
+          src={portfolio.portfolio_thumbnail}
+          alt="포트폴리오 섬네일"
+          onClick={handlePortfolioConfirm}
+        />
       ) : (
         ''
       )}
-      <div className={styles.textContainer}>
+      <div className={styles.textContainer} onClick={handlePortfolioConfirm}>
         <div className={styles.titleBox}>
           <p className={styles.title}>{portfolio.portfolio_title}</p>
           {getIsNew(portfolio.portfolio_created_at) && <p className={styles.new}>NEW</p>}
@@ -51,17 +87,19 @@ function PortfolioCell({
               return <div key={stack}>{stack}</div>;
             })}
           {portfolio.portfolio_stacks?.stackList.length > 3 && (
-            <span className={styles.ellipsis}>…</span>
+            <span className={styles.ellipsis}>
+              + {portfolio.portfolio_stacks?.stackList.length}
+            </span>
           )}
         </div>
-        <p className={styles.informations}>
-          <span>👀</span>
-          <span>{portfolio.portfolio_views_count}</span>
-          <span>💬</span>
-          <span>{portfolio.portfolio_comments_count}</span>
-          <span>📌</span>
-          <span>{portfolio.portfolio_bookmark_count}</span>
-        </p>
+      </div>
+      <div className={styles.informations}>
+        <span>👀</span>
+        <span>{portfolio.portfolio_views_count}</span>
+        <span>💬</span>
+        <span>{portfolio.portfolio_comments_count}</span>
+        <span>📌</span>
+        <span>{portfolio.portfolio_bookmark_count}</span>
       </div>
     </div>
   );
