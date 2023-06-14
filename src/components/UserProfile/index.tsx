adda42c864939a85a08e586880c0aa937ab374ff
hyzzzzy <shinhj4068@naver.com>
2023-06-14 23:01:20 +0900

    Fix: 다른 유저 페이지 마이페이지와 UI 통일

--- src/components/UserProfile/index.tsx
+++ src/components/UserProfile/index.tsx
@@ -5,6 +5,7 @@ import { TypeUserProfile } from '../../interfaces/User.interface';
 import { getUserProfileById } from '../../apis/Fetcher';
 import ROUTES from '../../constants/Routes';
 import DefaultUserImg from '../../assets/DefaultUser.png';
+import { FcBriefcase, FcSupport } from "react-icons/fc";
 
 function Profile() {
   const params: { [key: string]: string | undefined } = useParams();
@@ -38,20 +39,23 @@ function Profile() {
       <div className={styles.introWrapper}>
         <div className={styles.name}>{user?.user_name}</div>
         <div className={styles.intro}>{user?.user_introduction}</div>
-        <div className={styles.career}>{user?.user_career_goal}</div>
-        <div className={styles.stacks}>
-          {user?.user_stacks === null ? (
-            <></>
-          ) : (
-            user?.user_stacks.stackList.map((stack, index) => {
-              return (
-                <div className={styles.stack} key={`${stack}-${index}`}>
-                  {stack}
-                </div>
-              );
-            })
-          )}
+        <div className={styles.career}>
+          <FcBriefcase/>
+          {user?.user_career_goal 
+          ? user?.user_career_goal 
+          : <div className={styles.emptyCareer}>{user?.user_name}님의 목표 직군이 비어있어요</div>}
         </div>
+        <div className={styles.stacks}>
+          <FcSupport />
+          {user?.user_stacks?.stackList && user?.user_stacks?.stackList?.length > 0 
+          ? user?.user_stacks?.stackList?.map((stack, index) => {
+            return (
+              <div className={styles.stack} key={`${stack}-${index}`}>
+                {stack}
+              </div>
+            )
+          }) : <div className={styles.emptyStack}>{user?.user_name}님의 기술 스택이 비어있어요</div>}
+      </div>
       </div>
     </div>
   );
