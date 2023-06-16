04ff64b835ce9634198fbac42fda47b965bb2b4a
JuYoung Lee <lee_ju_0@naver.com>
2023-06-16 17:22:17 +0900

    Style: 모집글 상세페이지, 자랑리스트 로딩시 흰부분 생기는거 수정

--- src/components/common/Loading/Loading.tsx
+++ src/components/common/Loading/Loading.tsx
@@ -8,7 +8,10 @@ const LOADING_LOGO_COLOR: string = '#95a4b0';
 // 로딩 중 로고
 export default function Loading() {
   return (
-    <div className={styles.container} style={{ maxWidth: 1024, margin: '76px auto' }}>
+    <div
+      className={styles.container}
+      style={{ maxWidth: 1024, minHeight: '100vh', margin: '76px auto' }}
+    >
       <BiDotsVertical
         size={LOADING_LOGO_SIZE}
         color={LOADING_LOGO_COLOR}
