dc67901ea30bc4cc7695f3bbc3b66fbc4ba785c8
junhui324 <junhuijang00@gmail.com>
2023-06-21 15:18:41 +0900

    Style: 포트폴리오 상세, 미리보기 페이지 반응형 구현

--- src/pages/PortfolioDetail/index.tsx
+++ src/pages/PortfolioDetail/index.tsx
@@ -1,7 +1,6 @@
-//import PortfolioDetailForm from '../../components/PortfolioDetailForm/PortfolioDetailForm';
-//import Comment from '../../components/Comment';
 import LoadingSpinner from '../../components/common/Loading/LoadingSpinner';
 import React, { Suspense } from 'react';
+import styles from './PortfolioDetail.module.scss';
 const PortfolioDetailForm = React.lazy(
   () => import('../../components/PortfolioDetailForm/PortfolioDetailForm')
 );
@@ -9,10 +8,12 @@ const Comment = React.lazy(() => import('../../components/Comment'));
 
 function PortfolioDetail() {
   return (
-    <Suspense fallback={<LoadingSpinner />}>
-      <PortfolioDetailForm />
-      <Comment />
-    </Suspense>
+    <div className={styles.container}>
+      <Suspense fallback={<LoadingSpinner />}>
+        <PortfolioDetailForm />
+        <Comment />
+      </Suspense>
+    </div>
   );
 }
 
