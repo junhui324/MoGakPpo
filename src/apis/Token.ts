9a6d2609e2ea9200ba331313bd8e44004f44a806
JuYoung Lee <lee_ju_0@naver.com>
2023-06-21 19:45:56 +0900

    Fix: RefreshToken이 삭제되는 버그 해결

--- src/apis/Token.ts
+++ src/apis/Token.ts
@@ -10,10 +10,15 @@ function getToken(): string | null {
   return cookie.load('accessToken');
 }
 
+// 쿠키에 저장된 엑세스 토큰만을 삭제합니다.
+function removeAccessToken(): void {
+  cookie.remove('accessToken', { path: '/' });
+}
+
 // 쿠키에 저장된 토큰을 삭제합니다.
 function removeToken(): void {
   cookie.remove('accessToken', { path: '/' });
-  cookie.remove('refreshToken', {path:'/'});
+  cookie.remove('refreshToken', { path: '/' });
 }
 
 // 쿠키에 저장된 리프레시 토큰을 반환합니다.
@@ -21,4 +26,4 @@ function getRefreshToken(): string | null {
   return cookie.load('refreshToken');
 }
 
-export { setToken, getToken, removeToken, getRefreshToken };
+export { setToken, getToken, removeAccessToken, removeToken, getRefreshToken };
